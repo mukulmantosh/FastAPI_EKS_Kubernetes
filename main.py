e762061dc7bc217f8098a3f021683a220c97d243
Mukul Mantosh <mukulmantosh@yahoo.com>
2021-08-25 04:30:15 +0530

    Feat: - Updated K8s with Redis & Celery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+import os
+
 from celery import Celery
+from fastapi import FastAPI
+
 from ecommerce.auth import router as auth_router
 from ecommerce.cart import router as cart_router
 from ecommerce.orders import router as order_router
@@ -13,10 +16,14 @@ app.include_router(product_router.router)
 app.include_router(cart_router.router)
 app.include_router(order_router.router)
 
+REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
+REDIS_PORT = os.getenv('REDIS_PORT', '6379')
+REDIS_DB = os.getenv('REDIS_DB', '0')
+
 celery = Celery(
     __name__,
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0"
+    broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}",
+    backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 )
 celery.conf.imports = [
     'ecommerce.orders.tasks',
