97b198b620259bf633bc0507d52364501f26faef
Mukul Mantosh <mukulmantosh@yahoo.com>
2021-09-16 10:17:10 +0530

    Feat: updated docs metadata

--- main.py
+++ main.py
@@ -8,7 +8,32 @@ from ecommerce.orders import router as order_router
 from ecommerce.products import router as product_router
 from ecommerce.user import router as user_router
 
-app = FastAPI(title='Sample Docs', version='1.0')
+
+description = """
+Ecommerce API
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+app = FastAPI(
+    title="EcommerceApp",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Mukul Mantosh",
+        "url": "http://x-force.example.com/contact/",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
 app.include_router(product_router.router)
